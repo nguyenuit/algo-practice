b02a14681e7272fe48815f3c23f9d0ef2119f05b
Nguyen <nguyenuit@gmail.com>
2023-01-29 11:19:03 +0700

    Rotate an Array by N Elements

---
+++ rotate-an-array-by-n-elements/function.js
@@ -0,0 +1,24 @@
+// # Rotate an Array by N Elements
+// Given an integer array, rotate it by 'n' elements.
+
+// ## Statement
+// We’re given an array of integers, nums. Rotate the array by n elements, where n is an integer:
+// For positive values of n, perform a right rotation.
+// For negative values of n, perform a left rotation.
+// Make sure we make changes to the original array.
+
+// ### Sample input
+// nums = [1, 10, 20, 0, 59, 86, 32, 11, 9, 40]
+// n = 2
+// Expected output
+// [9, 40, 1, 10, 20, 0, 59, 86, 32, 11]
+
+function solveProblem(){
+
+}
+
+let input = [1, 10, 20, 0, 59, 86, 32, 11, 9, 40];
+let n = 2;
+let output = solveProblem(input, n);
+// [9, 40, 1, 10, 20, 0, 59, 86, 32, 11]
+console.log(output);
