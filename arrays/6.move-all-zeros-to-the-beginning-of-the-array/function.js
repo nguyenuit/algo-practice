36e8b117efcabe6960bc65414e7a20a1d5d5644d
Nguyen <nguyenuit@gmail.com>
2023-01-31 18:43:02 +0700

    Move All Zeros to the Beginning of the Array, finish it

--- arrays/6.move-all-zeros-to-the-beginning-of-the-array/function.js
+++ arrays/6.move-all-zeros-to-the-beginning-of-the-array/function.js
@@ -11,38 +11,26 @@
 // [0, 0, 0, 1, 10, 20, 59, 63, 88]
 
 function solveProblem(nums){
-  let read = nums.length - 1;
-  let write = nums.length - 1;
+  let read = parseInt(nums.length) - 1;
+  let write = parseInt(nums.length) - 1;
 
   while(read >= 0){
-    
-    if (read != 0){
-
-      if (nums[read] != 0){
-        read--;
-        write--;
-      }else{
-        // what if nums[read-1] == 0
-        nums[write] = nums[read-1];
-        read--;
-      }
+    if (nums[read] != 0){
+      nums[write] = nums[read];
+      read--;
+      write--;
     }else{
-      if (nums[read] != 0){
-        nums[write] = nums[read];
-        read--;
-      }
+      read--;
     }
   }
-  
+
   while(write >= 0){
-    write--;
     nums[write] = 0;
+    write--;
   }
 
   return nums;
 }
-let input = [1,2,0,3,0,4,7];
-let output = solveProblem(input);
-
+const input = [1, 10, 20, 0, 59, 63, 0, 88, 0];
+const output = solveProblem(input);
 console.log(output);
-// [0,0,1,2,3,4,7]
