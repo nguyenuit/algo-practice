cc0814446ecbc1b55c1ef16a66475e8ce790604e
Nguyen <nguyenuit@gmail.com>
2023-01-31 18:20:53 +0700

    Move All Zeros to the Beginning of the Array, improve it

--- arrays/6.move-all-zeros-to-the-beginning-of-the-array/function.js
+++ arrays/6.move-all-zeros-to-the-beginning-of-the-array/function.js
@@ -1,31 +1,45 @@
-function solveProblem(input){
-  let j = parseInt(input.length) - 1;
-  let i = parseInt(input.length) - 1;
-  while(j >= 0){
-    
-    if (j!=0){
+// # Move All Zeros to the Beginning of the Array
+// Move all zeros to the left of an array while maintaining its order.
 
-      if (input[j] != 0){
-        j--;
-        i--;
+// ## Statement
+// We’re given an integer array, nums. Move all zeroes, if any, to the left while maintaining the order of other elements in the array.
+// All changes must be made in nums itself; no return value is expected.
+
+// ### Sample input
+// [1, 10, 20, 0, 59, 63, 0, 88, 0]
+// Expected output
+// [0, 0, 0, 1, 10, 20, 59, 63, 88]
+
+function solveProblem(nums){
+  let read = nums.length - 1;
+  let write = nums.length - 1;
+
+  while(read >= 0){
+    
+    if (read != 0){
+
+      if (nums[read] != 0){
+        read--;
+        write--;
       }else{
-        input[i] = input[j-1];
-        j--;
+        // what if nums[read-1] == 0
+        nums[write] = nums[read-1];
+        read--;
       }
     }else{
-      if (input[j] != 0){
-        input[i] = input[j];
-        j--;
+      if (nums[read] != 0){
+        nums[write] = nums[read];
+        read--;
       }
     }
   }
   
-  while(i>=0){
-    i--;
-    input[i] = 0;
+  while(write >= 0){
+    write--;
+    nums[write] = 0;
   }
 
-  return input;
+  return nums;
 }
 let input = [1,2,0,3,0,4,7];
 let output = solveProblem(input);
