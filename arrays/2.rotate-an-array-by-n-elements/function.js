45253649fa526e7c7aba7011deaade1f0e692d61
Nguyen <nguyenuit@gmail.com>
2023-02-02 09:30:38 +0700

    Rotate an Array by N Elements

--- arrays/2.rotate-an-array-by-n-elements/function.js
+++ arrays/2.rotate-an-array-by-n-elements/function.js
@@ -13,12 +13,35 @@
 // Expected output
 // [9, 40, 1, 10, 20, 0, 59, 86, 32, 11]
 
-function solveProblem(input, n){
+function solveProblem(nums, n){
+    
+    let x = nums.length - n;
+    let y = nums.length - 1;
 
+    for (let i = x; i < nums.length - 1; i++){
+        let tmp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = tmp;
+        y--;
+    }
+
+    y = x - 1;
+    for (let i = 0; i < y; i++){
+        let tmp = nums[i];
+        nums[i] = nums[y];
+        nums[y] = tmp;
+        y--;
+    }
+
+    let nums2 = [];
+    for(let i = nums.length -1; i >= 0; i--){
+        nums2.push(nums[i]);
+    }
+
+    return nums2;
 }
 
 let input = [1, 10, 20, 0, 59, 86, 32, 11, 9, 40];
 let n = 2;
 let output = solveProblem(input, n);
-// [9, 40, 1, 10, 20, 0, 59, 86, 32, 11]
 console.log(output);
