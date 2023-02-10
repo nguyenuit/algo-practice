40f4dd984b287f1919b4e0b7bbde9504451c116d
Nguyen <nguyenuit@gmail.com>
2023-02-10 14:39:31 +0700

    Sort Colors

---
+++ arrays/13.sort-colors/function.js
@@ -0,0 +1,24 @@
+// # Sort Colors
+// Given an array of n colored objects, sort the array in place so that objects of the same color are adjacent, with the colors in a specific order.
+
+// ## Statement
+// Given an array of n objects colored white, red, or blue, sort the array in place so that objects of the same color are adjacent,
+// with the colors in the order red, white, and blue.
+// This problem is known as Dutch National Flag Problem. The idea is to attribute a color to each number and then arrange them following the order of colors on the Dutch flag.
+// We’ll use the integers 0, 1, and 2 to represent red, white, and blue, respectively.
+
+// ### Constraints
+// Input numbers are restricted to 0, 1, or 2.
+
+// #### Sample input
+// [2, 0, 2, 1, 1, 0]
+// Expected output
+// [0, 0, 1, 1, 2, 2]
+
+function solveProblem(){
+
+}
+
+const input = [2, 0, 2, 1, 1, 0];
+const output = solveProblem(input);
+console.log(output);
